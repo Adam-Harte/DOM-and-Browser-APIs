58fe4fa43207be1094dd5346618adfdd470418d2
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-09 22:40:24 +0000

    DOM queries added to select elements added

---
+++ app.js
@@ -0,0 +1,6 @@
+//selecting elements
+document.getElementById('main-title');
+document.querySelector('ul');
+document.getElementsByClassName('list-item');
+document.querySelectorAll('.list-item');
+document.querySelector('ul li:last-of-type');
