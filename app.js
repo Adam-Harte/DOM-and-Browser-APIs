b6814fe1183e395c99472033cfab5b4662cae929
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-10 23:10:17 +0000

    manipulating elements properties and attributes added

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ document.querySelector('ul');
 document.getElementsByClassName('list-item');
 document.querySelectorAll('.list-item');
 document.querySelector('ul li:last-of-type');
+
+//manipulating elements
+const title = document.querySelector('h1');
+title.textContent = 'Changed title';
+title.className = 'title';
+title.style.backgroundColor = 'red';
+title.style.color = 'white';
+title.setAttribute('id', 'new-title');
